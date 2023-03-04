b234fee18a15328799ed71ac24632af6646693c6
lea-stellini <lea.stellini@gmail.com>
2023-03-04 16:47:09 +0100

    ajout modal

--- js/script.js
+++ js/script.js
@@ -1,14 +1,18 @@
 import { navbar } from "./navbar.js";
 import { globalConfig } from "../config.js";
 
+
 // récupération des éléments du DOM 
 const gallery = document.getElementById('gallery');
+const galleryEdit = document.getElementById('galleryEdit')
 const login = document.getElementById("loginNav");
 const logout = document.getElementById("logoutNav"); 
 const editBar = document.getElementById("edit");
 const editImg = document.getElementById("editImg");
 const editArticle = document.getElementById("editArticle");
 const editWorks = document.getElementById("editWorks");
+const modal = document.getElementById("modal");
+const closeBtn = document.getElementById("close");
 
 // récupére les éléments de l'api
 const work = async () => {
@@ -16,7 +20,7 @@ const work = async () => {
    return data;
 }
 
-// affiche les éléments
+// affiche les éléments sur la page projets
 const displayInfos = (infos) => {
 
   document.getElementById('gallery').innerHTML = "";
@@ -37,6 +41,34 @@ const displayInfos = (infos) => {
     figure.appendChild(figcaption);
     gallery.appendChild(figure);
   } 
+}
+
+// affiche les éléments sur la galerie photo
+const displayPhoto = (infos) => {
+  document.getElementById('galleryEdit').innerHTML = "";
+
+  for(let i = 0; i < infos.length; i++){
+    const div = document.createElement("div");
+    const button = document.createElement("button");
+    const img = document.createElement("img");
+    const p = document.createElement("p");
+    const icon = document.createElement('img');
+
+    img.src = infos[i].imageUrl;
+    img.alt = infos[i].title;
+    img.crossOrigin = "anonymous";
+    img.className = 
+    p.innerText = "éditer";
+    icon.src = "../assets/icons/delete.svg";
+    button.className = 'btnIcon';
+    div.className = 'card';
+
+    button.appendChild(icon)
+    div.appendChild(img);
+    div.appendChild(button)
+    div.appendChild(p);
+    galleryEdit.appendChild(div);
+  }
 }
 
 // filtre les éléments
@@ -75,6 +107,14 @@ const main = async () => {
   })
 }
 
+// Afficher les projets dans la galerie photo
+
+const mainModal = async () => {
+  const infos = await work();
+
+  displayPhoto(infos);
+}
+
 // se déconnecter 
 logout.addEventListener('click', () => {
   localStorage.clear()
@@ -93,8 +133,24 @@ if(localStorage.getItem("token")){
     editWorks.classList.remove("noedit");
 }
 
+// affichage de la modale
+editWorks.addEventListener('click', () => {
+  modal.classList.remove("noedit");
+  modal.classList.add("edit");
+  modal.classList.add("flex");
+})
+
+// fermer la modale
+closeBtn.addEventListener('click', () => {
+  modal.classList.remove("edit");
+  modal.classList.remove("flex");
+  modal.classList.add("noedit");
+})
+
+
 
         
 
 navbar();
-main()
+main();
+mainModal();
